140fa1b1117d99cda8b904266dc025bd6c77ff84
david wee <rook2pawn@gmail.com>
2011-08-15 03:17:15 -0700

    removed files

--- example.js
+++
@@ -1,9 +0,0 @@
-var EventEmitter = require('events').EventEmitter;
-var Qlib = require('queuelib');
-var myEmitter = new EventEmitter;
-var myWorkFunction = function(el) { console.log(el + " is great!"); myEmitter.emit('next'); };
-var q = Qlib({work:myWorkFunction, emitter:myEmitter});
-
-q.push('NodeJS');
-q.push('DNode');
-
