99112854437cb2ac54f33a9c19de25ba91e19e5a
David Wee <rook2pawn@gmail.com>
2016-09-28 02:02:36 -0700

    updated readme, tabs

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
 [![Build Status](https://travis-ci.org/rook2pawn/node-queuelib.svg?branch=master)](https://travis-ci.org/rook2pawn/node-queuelib)
 
-New!
-===
+Example
+=======
+
+    var ql = require('queuelib');
+    var q = new ql;
+
+    var iterator = function(num, idx, lib) {
+      console.log(num*2);
+      lib.done();
+    };
+    var alldone = function(err) {
+      // all done
+      if (err)
+        throw err
+      console.log("All done!")
+    };
+
+    q.forEach([1,2,3], iterator, alldone)
+
+    // 2
+    // 4
+    // 6 
+    // All done!
+
+
+
+.forEach(list,iterator,alldone,ratelimit)
+-----------------------------------------
+
+Optional alldone function to be executed after entire list is processed.
+Optional ratelimit takes a positive integer that specifies the number of
+milliseconds between iterations.
+
+
+Rate Limiting
+-------------
+
 Rate Limiting. Just supply the millisecond after .forEach or .series
 
     q.series(<list>,<padding>);
@@ -11,64 +46,45 @@ Rate Limiting. Just supply the millisecond after .forEach or .series
     q.forEach(<list>,<iterator>,<all done>, <padding>)
 
 
-New!
-==== 
-ForEach
+memory key-value store across series or forEach
+-----------------------------------------------
 
-q.forEach(list,iterator,alldone)
---------------------------------------------------
-
-    q.forEach(['c','a','t'],function(val,idx,lib) {
-        console.log(val, idx);
-        lib.done() // signal next
-    }, function() {
-        console.log("all done");
-    });
-
-    // c 0
-    // a 1 
-    // t 2
-
-
-New!
-====
-Key value store across series! Just call .done(hash) to store the keys/values of the hash
+Just call .done(hash) to store the keys/values of the hash
 
     queue.series([
-        function(lib) {
-            lib.done({one:1})
-        },
-        function(lib) {
-            lib.done({two:2})
-        },
-        function(lib) {
-            var x = lib.get('one') + lib.get('two'); // x = 3
-            lib.done();
-        }
+      function(lib) {
+          lib.done({one:1})
+      },
+      function(lib) {
+          lib.done({two:2})
+      },
+      function(lib) {
+          var x = lib.get('one') + lib.get('two'); // x = 3
+          lib.done();
+      }
     ]);
 
 
 You can also use .set(hash)
 
     queue.series([
-        function(lib) {
-            lib.set({one:1});
-            lib.done()
-        },
-        function(lib) {
-            lib.set({life:42})
-            lib.done()
-        },
-        function(lib) {
-            t.equal(43,lib.get('one') + lib.get('life'));
-            lib.done();
-        }
+      function(lib) {
+          lib.set({one:1});
+          lib.done()
+      },
+      function(lib) {
+          lib.set({life:42})
+          lib.done()
+      },
+      function(lib) {
+          assert.equal(43,lib.get('one') + lib.get('life'));
+          lib.done();
+      }
     ]);
 
 
-New!
-====
-Early termination flow control in .series! 
+Early termination flow control
+-------------------------------
         
     queue.series([
     function(lib) {
@@ -103,82 +119,67 @@ Methods
     var queue = new Q;
     
     queue.pushAsync(function(lib) {
-        // do something asynchronously
-        lib.done();
+      // do something asynchronously
+      lib.done();
     });
     
     queue.pushAsync(function(lib) {
-        // do something else asynchronously
-        lib.done();
+      // do something else asynchronously
+      lib.done();
     });
 
 .series ([fn1,fn2,..])
 ======================
 
     queue.series([
-        function(lib) {
-            // do something asynchronously
-            lib.done();
-        },
-        function(lib) {
-            // do something else asynchronously
-            lib.done();
-        }
+      function(lib) {
+          // do something asynchronously
+          lib.done();
+      },
+      function(lib) {
+          // do something else asynchronously
+          lib.done();
+      }
     ]);
 
 You can also bind a context for any function in the series by declaring an object with a "fn" parameter and a "context" parameter like so: 
 
-
     queue.series([
-        {
-            fn:function(lib) {
-                // do something asynchronously
-                var x = this.a + this.b; // 42
-                lib.done();
-            },
-            context: {a:30, b:12}
-        }
-        function(lib) {
-            // do something else asynchronously
+      {
+        fn:function(lib) {
+            // do something asynchronously
+            var x = this.a + this.b; // 42
             lib.done();
-        }
+        },
+        context: {a:30, b:12}
+      }
+      function(lib) {
+        // do something else asynchronously
+        lib.done();
+      }
     ]);
 
 
 Example 1
 ---------
 
-
     var Q = require('queuelib');
     var request = require('request');
     var queue = new Q;
     
     queue.pushAsync(function(lib) {
-        // do something asynchronously
-        request('http://google.com',function(err,response,body) {
-            console.log(body);
-            lib.done();
-        });
+      // do something asynchronously
+      request('http://google.com',function(err,response,body) {
+        console.log(body);
+        lib.done();
+      });
     });
     
     queue.pushAsync(function(lib) {
-        // do something else asynchronously
-        request('http://reddit.com',function(err,response,body) {
-            console.log(body);
-            lib.done();
-        });
-        lib.done();
+      // do something else asynchronously
+      request('http://reddit.com',function(err,response,body) {
+          console.log(body);
+          lib.done();
+      });
+      lib.done();
     });
-
-
-.forEach (list, iterator, alldone) 
-==================================
-
-    var list = [1,2,3]
-    var sum = 0;
-    queue.forEach(list,function(item,idx,lib) {
-        sum += item;
-        lib.done()
-    },function() {
-        console.log(sum) // 6
-    })
--- index.js
+++ index.js
@@ -5,141 +5,141 @@ exports = module.exports = qlib;
 function qlib() {
 	this.queue = [];
 	this.working = false;
-    this.workAsync = function() {
-        var item = this.queue.shift();
-        var x = function() {
-            var self = this;
-            self._id = item.id;
-            self.terminate = this.terminate.bind(self)
-            if ((item.arg !== undefined) && (item.idx !== undefined))
-                item.fn.apply({},[item.arg,item.idx,self]);
-            else 
-                item.fn.apply({},[self]);
-        };
-        if (item !== undefined) {
-            this.working = true;
-            if ((item.padding !== undefined) && (item.padding > 0)) {
-                setTimeout(x.bind(this), item.padding)
-            } else {
-                x.call(this)
-            }
+  this.workAsync = function() {
+    var item = this.queue.shift();
+    var x = function() {
+      var self = this;
+      self._id = item.id;
+      self.terminate = this.terminate.bind(self)
+      if ((item.arg !== undefined) && (item.idx !== undefined))
+        item.fn.apply({},[item.arg,item.idx,self]);
+      else 
+        item.fn.apply({},[self]);
+    };
+    if (item !== undefined) {
+      this.working = true;
+      if ((item.padding !== undefined) && (item.padding > 0)) {
+        setTimeout(x.bind(this), item.padding)
+      } else {
+        x.call(this)
+      }
 		} 
 	};
 	this.pushAsync = function(el,fn,idx,id) {
-        if ((arguments.length == 1) && (typeof el == 'function')) {
-		    this.queue.push({fn:el,type:'async'});
-        } else if (arguments.length == 2)
-            this.queue.push({el:el,fn:fn,type:'async'});
-        else if (arguments.length == 4)
-            this.queue.push({el:el,fn:fn,idx:idx,id:id,type:'async'});
-        //console.log("Push Async:", arguments);
-        //console.log("this.queue.length:", this.queue.length)
-        //console.log("this.working:", this.working)
+    if ((arguments.length == 1) && (typeof el == 'function')) {
+    this.queue.push({fn:el,type:'async'});
+    } else if (arguments.length == 2)
+        this.queue.push({el:el,fn:fn,type:'async'});
+    else if (arguments.length == 4)
+        this.queue.push({el:el,fn:fn,idx:idx,id:id,type:'async'});
+    //console.log("Push Async:", arguments);
+    //console.log("this.queue.length:", this.queue.length)
+    //console.log("this.working:", this.working)
 		if ((this.queue.length > 0) && (this.working == false)) {
-            //console.log("WORKING ASYNC")
+      //console.log("WORKING ASYNC")
 			this.workAsync();
 		}
 		return this;
 	};
-    this.workSync = function() {
-        var item = this.queue.shift();
-        if (item !== undefined) {
-            this.working = true;
-			var fn = item.fn;
-            if (item.el === undefined)
-			    fn.apply(fn,[this]);
-            else 
-    			fn.apply(fn,[item.el,this]);
-        } 
-        this.done()
-    };
-    this.pushSync = function(el,fn) {
-        if ((arguments.length == 1) && (typeof el == 'function')) {
-		    this.queue.push({fn:el,type:'sync'});
-        } else 
-            this.queue.push({el:el,fn:fn,type:'sync'});
-        if ((this.queue.length > 0) && (this.working == false)) {
-            this.workSync();
-        }
+  this.workSync = function() {
+      var item = this.queue.shift();
+      if (item !== undefined) {
+          this.working = true;
+    var fn = item.fn;
+          if (item.el === undefined)
+        fn.apply(fn,[this]);
+          else 
+        fn.apply(fn,[item.el,this]);
+      } 
+      this.done()
+  };
+  this.pushSync = function(el,fn) {
+    if ((arguments.length == 1) && (typeof el == 'function')) {
+      this.queue.push({fn:el,type:'sync'});
+    } else 
+      this.queue.push({el:el,fn:fn,type:'sync'});
+    if ((this.queue.length > 0) && (this.working == false)) {
+      this.workSync();
     }
-    this.hash = {};
-    this.get = function(key) {
-        return this.hash[key];
+  }
+  this.hash = {};
+  this.get = function(key) {
+    return this.hash[key];
+  }
+  this.set = function(obj) {
+    if ((obj) && (typeof obj == 'object')) {
+      Hash(this.hash).update(obj);
     }
-    this.set = function(obj) {
-        if ((obj) && (typeof obj == 'object')) {
-            Hash(this.hash).update(obj);
-        }
-        return true;
-    }
-    var getCount = function(list,id) {
-        var count = 0;
-        list.forEach(function(item) {
-            if ((item.id) && (item.id == id))
-                count++
-        })
-        return count
-    }
+    return true;
+  }
+  var getCount = function(list,id) {
+    var count = 0;
+    list.forEach(function(item) {
+      if ((item.id) && (item.id == id))
+        count++
+    })
+    return count
+  }
 	this.done = function(obj) {
-        // generally
-        if ((obj) && (typeof obj == 'object')) {
-            Hash(this.hash).update(obj);
-        }
-        this.working = false;
-        if (this.queue.length > 0) {
-            var item = this.queue[0];
-            if (item.type == 'sync')
-                this.workSync();
-            else if (item.type == 'async')
-                this.workAsync();
-        } 
+    // generally
+    if ((obj) && (typeof obj == 'object')) {
+      Hash(this.hash).update(obj);
+    }
+    this.working = false;
+    if (this.queue.length > 0) {
+      var item = this.queue[0];
+      if (item.type == 'sync')
+        this.workSync();
+      else if (item.type == 'async')
+        this.workAsync();
+    } 
 	};
-    this.terminate = function() {
-        var id = this._id
-        var tmp = [];
-        for (var i = 0; i < this.queue.length; i++) {
-            var item = this.queue[i];
-            if ((item.id !== undefined) && (item.id == id)) {
-                // unsaved
-            } else {
-                tmp.push(item);
-            }
-        }
-        this.queue = tmp;
-        this.done();
-    };
-    var gen_id = function() {
-        return String.fromCharCode(~~(Math.random() * 26) + 97).concat((Math.random()+1).toString(36).substr(2,5))
+  this.terminate = function() {
+    var id = this._id
+    var tmp = [];
+    for (var i = 0; i < this.queue.length; i++) {
+      var item = this.queue[i];
+      if ((item.id !== undefined) && (item.id == id)) {
+        // unsaved
+      } else {
+        tmp.push(item);
+      }
     }
-    this.series = function(list,padding) {
-        if (padding === undefined) 
-            padding = 0
-        var id = gen_id()
-        list.forEach(function(fn) {
-            if ((typeof fn === 'object') && (fn.fn) && (fn.context))
-                this.queue.push({fn:fn.fn.bind(fn.context),type:'async',id:id});
-            else 
-                this.queue.push({fn:fn,type:'async',id:id});
-        },this);
-        if ((this.queue.length > 0) && (this.working == false)) {
-            this.workAsync();
-        }
-        return id
+    this.queue = tmp;
+    this.done();
+  };
+  var gen_id = function() {
+    return String.fromCharCode(~~(Math.random() * 26) + 97).concat((Math.random()+1).toString(36).substr(2,5))
+  }
+  this.series = function(list,padding) {
+    if (padding === undefined) 
+        padding = 0
+    var id = gen_id()
+    list.forEach(function(fn) {
+        if ((typeof fn === 'object') && (fn.fn) && (fn.context))
+            this.queue.push({fn:fn.fn.bind(fn.context),type:'async',id:id});
+        else 
+            this.queue.push({fn:fn,type:'async',id:id});
+    },this);
+    if ((this.queue.length > 0) && (this.working == false)) {
+        this.workAsync();
     }
-    this.forEach = function(list, iterator, done, padding) {
-        var id = gen_id()
-        if (done === undefined)
-            done = function() { }
-        if (padding === undefined) 
-            padding = 0
-        if (list && list.length) 
-            list.forEach(function(arg,idx) {
-                this.queue.push({fn:iterator,type:'async',idx:idx,id:id,arg:arg,padding:padding})
-                if (idx == list.length - 1)
-                    this.queue.push({fn:done, idx:idx,id:id,type:'sync'})
-                if ((this.queue.length > 0) && (this.working == false))
-                    this.workAsync();
-            },this)
-        else done()
-    }
+    return id
+  }
+  this.forEach = function(list, iterator, done, padding) {
+    var id = gen_id()
+    if (done === undefined)
+        done = function() { }
+    if (padding === undefined) 
+        padding = 0
+    if (list && list.length) 
+      list.forEach(function(arg,idx) {
+          this.queue.push({fn:iterator,type:'async',idx:idx,id:id,arg:arg,padding:padding})
+          if (idx == list.length - 1)
+              this.queue.push({fn:done, idx:idx,id:id,type:'sync'})
+          if ((this.queue.length > 0) && (this.working == false))
+              this.workAsync();
+      },this)
+    else done()
+  }
 };
