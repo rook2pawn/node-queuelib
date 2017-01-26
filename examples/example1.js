92409cb794f06ce54f67a961579b5f2aeba39b34
rook2pawn <rook2pawn@gmail.com>
2017-01-26 11:09:19 -0800

    updated examples

--- examples/example1.js
+++ examples/example1.js
@@ -1,39 +1,10 @@
-var Q = require('../');
-var request = require('request');
-var queue = new Q;
+var QL = require('../');
 
-queue.pushAsync(function(lib) {
-    // do something asynchronously
-    console.log("getting google");
-    request('http://google.com',function(err,response,body) {
-        console.log(response.request.host);
-        lib.done();
-    });
-});
-
-queue.pushAsync(function(lib) {
-    // do something else asynchronously
-    console.log("getting reddit");
-    request('http://reddit.com',function(err,response,body) {
-        console.log(response.request.host);
-        lib.done();
-    });
-});
-
-
-queue.series([
-    function(lib) {
-        console.log("getting xkcd");
-        request('http://xkcd.com',function(err,response,body) {
-            console.log(response.request.host);
-            lib.done();
-        });
-    },
-    function(lib) {
-        console.log("getting nmpjs");
-        request('http://npmjs.org',function(err,response,body) {
-            console.log(response.request.host);
-            lib.done();
-        });
-    }
-]);
+var q = new QL;
+var sum = 0;
+q.forEach([1,2,3],function(val,idx,lib) {
+  sum += val;
+  lib.done();
+},function() {
+  console.log(sum)
+})
